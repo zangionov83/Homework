b6aa52f0c8e15b19a220492e56a83576db5e3a00
Александр Зан <zangionov83@gmail.com>
2024-12-08 16:36:20 +0400

    Выполнение домашних заданий 08/12/2024

---
+++ Urban/Модуль 1  Практика GIT_Базовые структуры данных/Module_1_2_Peremennie.py
@@ -0,0 +1,5 @@
+homework = 12
+all_hours = 1.5
+name_course = 'Python'
+time_spent = all_hours / homework
+print(f"Курс: {name_course}, всего задач: {homework}, затрачено часов: {all_hours}, среднее время выполнения: {time_spent}")
